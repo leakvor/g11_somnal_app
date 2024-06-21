a01f80f8230cecac6f7cdc1aff2e569c04b3665a
sreyvoath <141004103+sreyvoath@users.noreply.github.com>
2024-06-21 08:20:52 +0700

    create service page

---
+++ frontend/src/views/Web/ContactUs/contactUsView.vue
@@ -0,0 +1,18 @@
+<template>
+    <WebLayout/>
+  
+</template>
+
+<script>
+import WebLayout from '@/Components/Layouts/WebLayout.vue'
+export default {
+    components:{
+        WebLayout
+    }
+
+}
+</script>
+
+<style>
+
+</style>
