61336dd67a529c2e03254395cf8e01d8055c8d85
sreyvoath <141004103+sreyvoath@users.noreply.github.com>
2024-06-27 10:36:44 +0700

    update on crude post interface #203

--- frontend/src/views/Web/Post/CreateView.vue
+++ frontend/src/views/Web/Post/CreateView.vue
@@ -22,7 +22,7 @@
         Cancel
       </a>
       <a
-        href="/list_post"
+        href="/update_post"
         class="px-4 py-2 bg-green-600 text-white font-semibold rounded-lg hover:bg-green-600 no-underline"
       >
         Save
