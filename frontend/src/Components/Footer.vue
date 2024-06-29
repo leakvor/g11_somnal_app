0717692720df58dcc98ab01383462f431caa1da8
Tiv-Nguot <tiv.nguot@student.passerellesnumeriques.org>
2024-06-29 13:44:09 +0700

    SCRUM-115 [feat] footer have reponsive

--- frontend/src/Components/Footer.vue
+++ frontend/src/Components/Footer.vue
@@ -2,35 +2,115 @@
 import { Icon } from '@iconify/vue'
 </script>
 <template>
-  <div class="footer d-flex p-14 bg-teal-900  color-white">
-    <div class="about-somnal mr-50">
-        <div class="logo d-flex ml-10 tex-center">
-            <!-- <img src="../assets/image/logo.png" alt="logo" class="w-10 h-10"> -->
-            <h5>About Somnal</h5>
+    <footer class="footer p-4" >
+        <div class="row">
+            <div class="col-md-3">
+                <h6>About Sonal</h6>
+                <p>
+                    Sonal Solution is a comprehensive platform that provides innovative and
+            sustainable solutions to address the challenges faced by individuals
+            and communities. Our mission is to empower people and create a better
+            future through the application of cutting-edge technology and
+            collaborative initiatives.
+                </p>
+            </div>
+            <div class="col-md-2">
+                <h6>Products</h6>
+                <ul class="list-unstyled">
+                    <li><router-link to="/adjay">Scrap</router-link></li>
+                    <li><router-link to="/drain">Scrap</router-link></li>
+                </ul>
+            </div>
+            <div class="col-md-3">
+                <h6>Useful Links</h6>
+                <ul class="list-unstyled">
+                    <li><router-link to="/" >Home</router-link></li>
+                    <li><router-link to="/service" >Service</router-link></li>
+                    <li><router-link to="about">About Us</router-link></li>
+                    <li><router-link  to="/">Our Partner</router-link></li>
+                    <li><router-link to="/">Register</router-link></li>
+                    <li><router-link to="/">Login</router-link></li>
+                </ul>
+            </div>
+            <div class="col-md-4">
+                <h6>Contact</h6>
+                <ul class="list-unstyled">
+                    <li><a href="#" class="d-flex align-items-center"><i class="material-icons pe-2">location_on</i> New York, NY 10012, US</a></li>
+                    <li><a href="#"class="d-flex align-items-center"><i class="material-icons pe-2">mail</i>info@gmail.com</a></li>
+                    <li><a href="#" class="d-flex align-items-center"><i class="material-icons pe-2">phone</i> (+855) 01 234 567 88</a></li>
+                </ul>
+            </div>
         </div>
-        <p>Whether it's a residential property, a commercial establishment, or scrap materials, [Your Company Name] is your go-to solution for all drain-related and scrap buying concerns. Contact us today through our website and experience the best in drain services.</p>
-    </div>
-    <div class="navigation ml-30 mr-50">
-        <h5>Navigation</h5>
-        <a href="#" class="color-white no-underline">Home</a> <br/>
-        <a href="#" class="color-white no-underline">Service</a><br/>
-        <a href="#" class="color-white no-underline">About Us</a><br/>
-        <a href="#" class="color-white no-underline">Contact US</a>
-    </div>
-    <div class="contact-us">
-        <h5>Contact Us</h5>
-        <div class="icon-contact flex items-center space-x-2">
-            <Icon icon="mdi-phone"/>
-            <p>+628123456789</p>
+        <hr class="my-3">
+        <div class="row">
+            <div class="col-md-7 text-center text-md-start">
+                <p>&copy; 2024 Sonal. All rights reserved.</p>
+            </div>
+            <div class="col-md-5 text-center text-md-end">
+                <div class="social-icons">
+                    <a class="btn btn-outline-light btn-floating m-1"><i class="fa-brands fa-facebook"></i></a>
+                    <a class="btn btn-outline-light btn-floating m-1"><i class="fa-brands fa-twitter"></i></a>
+                    <a class="btn btn-outline-light btn-floating m-1"><i
+                            class="fa-brands fa-instagram"></i></a>
+                    <a class="btn btn-outline-light btn-floating m-1"><i
+                            class="fa-brands fa-linkedin-in"></i></a>
+                </div>
+            </div>
         </div>
-        <div class="icon-contact">
-
-        </div>
-    </div>
-  </div>
+    </footer>
 </template>
 
 
-<style>
+<style scoped>
+.footer {
+    background-color: #212529;
+    color: white;
+    padding-top: 3rem;
+}
 
+.footer a {
+    color: #adb5bd;
+    text-decoration: none;
+    transition: color 0.3s ease-in-out, transform 0.3s ease-in-out;
+}
+
+.footer a:hover {
+    color: #de8e17;
+}
+.social-icons a:hover {
+    transform: translateX(5px);
+}
+
+.footer h6 {
+    color: #ffffff;
+    margin-bottom: 1rem;
+    font-weight: bold;
+}
+
+.footer ul li {
+    margin-bottom: 0.5rem;
+}
+
+.footer .btn-floating {
+    border-radius: 50%;
+    transition: all 0.3s ease-in-out;
+}
+
+.footer .btn-floating:hover {
+    background-color: #495057;
+    color: white;
+}
+
+.footer .social-icons a {
+    color: #adb5bd;
+}
+
+.footer .social-icons a:hover {
+    color: #ffffff;
+    background-color: #343a40;
+}
+
+.footer hr {
+    border-top: 1px solid #495057;
+}
 </style>
