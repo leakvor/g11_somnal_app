b78b3c1ebac763f46c949bc4c8ed48232059714d
Tiv-Nguot <tiv.nguot@student.passerellesnumeriques.org>
2024-07-05 11:18:44 +0700

    SCRUM-159 [imp] change slider and add list scrap and company card

--- frontend/src/views/Web/HomePage/HomePage.vue
+++ frontend/src/views/Web/HomePage/HomePage.vue
@@ -1,212 +1,244 @@
 <template>
-  <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel" data-bs-interval="1500">
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img src="https://www.euractiv.com/wp-content/uploads/sites/2/2020/08/Plastic_bottles-800x450.jpg" class="d-block w-100 h-100" alt="Carousel Image 1"/>
+  <div class="pb-5">
+    <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
+          aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+          aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+          aria-label="Slide 3"></button>
       </div>
-      <div class="carousel-item">
-        <img src="https://wallpapers.com/images/hd/peaceful-background-ia029sdh1de3pwu9.jpg" class="d-block w-100 h-100" alt="Carousel Image 2"/>
-      </div>  
-      <div class="carousel-item">
-        <img src="https://wallpapers.com/images/hd/peaceful-background-aymk14ikibr03mwt.jpg" class="d-block w-100 h-100" alt="Carousel Image 3"/>
+      <div class="carousel-inner">
+        <div class="carousel-item active" data-bs-interval="2000">
+          <img
+            src="../../../assets/Slider/slider-1.png"
+            class="d-block w-100" alt="..." />
+        </div>
+        <div class="carousel-item" data-bs-interval="2000">
+          <img
+            src="../../../assets/Slider/slider-2.png"
+            class="d-block w-100" alt="..." />
+        </div>
+        <div class="carousel-item">
+          <img
+            src="../../../assets/Slider/slider-3.png"
+            class="d-block w-100" alt="..." />
+        </div>
       </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval"
+        data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval"
+        data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
     </div>
-    <div class="position-absolute top-0 start-0 h-100 d-md-block" id="bg-blur">
-      <div class="position-absolute top-30 start-10 w-50 text-white bg-secondary p-5 text-dark bg-opacity-75"  id="misson" >
-        <h2>SONAL SOLUTION</h2>
-        <h2 id="blur-text">
-          Somnal Solution is a comprehensive platform that provides innovative and
-          sustainable solutions to address the challenges faced by individuals
-          and communities. Our mission is to empower people and create a better
-          future through the application of cutting-edge technology and
-          collaborative initiatives.
-        </h2>
-      </div>
-    </div>
-  </div>
-  <div class="container">
-    <div class="d-flex justify-content-between mt-20">
-      <div class="col-12 col-md-4 bg-success shadow rounded mb-4 mb-md-0" id="first_child">
-        <h2 class="text-center mt-3 text-orange" >How to use!</h2>
-        <div class="ml-3 mt-4 text-white">
-          <h6>1. First you just register for get the Account.</h6>
-          <h6>2. If you havealready an account,you can log in.</h6>
-          <h6>3. When you log in, you can use our app.</h6>
-          <h6>4. You can post your scraps here. by click on button pot in your account</h6>
-          <h6>5. You can chat and commend with the companies that you want to shales.</h6>
-          <h6>6. then you can select the company to whom you want to shale your scraps.</h6>
-          <h6>7. You can also view the scraps that you have posted and the companies that you have shale them.</h6>
-          <h6>7. Not only, can also view the scraps that you have posted and the companies that you have shale them.</h6>
 
-        </div>
-      </div>
-      <div class="col-12 col-md-7">
-        <img src="https://insidesmallbusiness.com.au/wp-content/uploads/2021/04/Small-businesses-using-apps-in-record-numbers.jpg" class="w-100 h-100 shadow rounded" id="imag-right" alt=""/>
-      </div>
+    <div class="button-group d-flex justify-content-end pt-5 pe-3">
+      <a href="" type="button" class="btn btn-success d-flex align-items-center me-3 p-3">
+        <i class="material-icons icon-align">add_shopping_cart</i>
+        <span>Sale Now</span>
+      </a>
+      <a href="" type="button" class="btn btn-primary text-white d-flex align-items-center"> 
+        <i class="material-icons icon-align">shopping_bag</i>
+        <span>Buy Now</span>
+      </a>
     </div>
-    <div class="mt-5 ">
-      <div class="d-flex justify-content-between mt-20">
-        <div class="col-md-7">
-          <img src="https://news.sophos.com/wp-content/uploads/2017/05/shutterstock_439303546-11.jpg" class="w-100 h-100 shadow rounded" id="img-r2" alt=""/>
-        </div>
-        <div class="col-md-4 bg-white shadow rounded mb-4 mb-md-0">
-          <h3 class="text-center mt-5 text-success" id="question">Why use Somnal App? </h3>
-          <h6 class="m4 text-black">
-            <b>Somnal App</b> offers a wide range of features that cater to both small and large businesses. With its easy-to-use interface and intuitive design, Somnal App makes it easy for individuals and businesses to manage their business operations.
-            <b>Somnal App also</b> allow the customers can post they scraps in Somnal app for shales them scraps and contact with companies to buy the scrap. And also in the somnal app you can contact to pumping your bathroom it can help you qickly for solve your problems Sewage blockage at your home.
 
-          </h6>
-        </div>
+    <div class="table-responsive bg-white  mt-5 pe-3 ps-3">
+      <h3 class="text-secondary font-bold pb-3 pt-2">Scrap Price List</h3>
+      <table class="table text-nowrap table-striped table-hover ">
+        <thead class="table-dark text-white">
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Scrap Name</th>
+            <th scope="col">Date</th>
+            <th scope="col">Fee</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(item, index) in items" :key="index">
+            <th scope="row">{{ index + 1 }}</th>
+            <td>{{ item.scrapName }}</td>
+            <td>{{ item.date }}</td>
+            <td ><span class="text-danger font-bold">${{ item.fee }}</span><span class="text-success">/kg</span></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
+    <div class="list-company mt-3 p-3 bg-white">
+      <h3 class="text-secondary font-bold pb-3">Top 5 Company</h3>
+      <div class=" d-flex justify-content-end pt-2 pb-4"><a href="" class=" btn btn-success text-decoration-none text-white text-right">See All...</a></div>
+      <div class="list-container pb-5">
+        <a v-for="(company, index) in companies" :key="index" href=""
+          class="card company-card text-decoration-none text-dark pe-3 ps-3 pb-3">
+          <div class="card-body">
+            <div class="company-logo text-center">
+              <img src="../../../assets/company-logo/company-logo.png" alt="Company Logo" />
+            </div>
+            <div class="company-info">
+              <h5 class="text-title">{{ company.name }}</h5>
+              <p class="text-card text-danger">{{ company.service }}</p>
+              <p class="text-card">{{ company.address }}</p>
+              <a :href="'tel:' + company.tel" class="text-card text-decoration-none">{{ company.tel }}</a>
+            </div>
+          </div>
+        </a>
       </div>
     </div>
   </div>
 </template>
 
-<style>
-
-#bg-blur {
-  background-size: cover;
-  background-position: center;
-  backdrop-filter: blur(5px);
-  filter: brightness(70%);
-  width: 100%;
-  height: 100vh;
-}
-#misson {
-  background-size: cover;
-  background-position: center;
-  backdrop-filter: blur(2px);
-  width: 100%;
-
-
-}
-.col-12.col-md-4,.col-md-4  {
-  border-bottom: 5px solid hwb(30 1% 15%);
-}
-
-#imag-right, #img-r2{
-  border-bottom: 5px solid hwb(152 1% 48%); 
-}
-
-
-.col-12.col-md-4,.col-12.col-md-7,.col-md-4,.col-md-7 {
-  transition: all 1s ease;
-}
-
-.col-12.col-md-4:hover,
-.col-12.col-md-7:hover,.col-md-4:hover,.col-md-7:hover {
-  transform: translateY(-20px);
-}
-
-
-/* Responsive styles */
-@media (max-width: 767px) {
-  .col-12.col-md-4,.col-md-4, .col-md-7, .col-12.col-md-7 {
-    transition: none;
-    margin: 0; /* Use valid margin declaration */
-  }
-
-  .col-12.col-md-4:hover,.col-md-4:hover, .col-md-7:hover, .col-12.col-md-7:hover {
-    transform: none;
-
-  }
-  #first_child{
-    position: relative;
-    top: 23px;
+<script>
+export default {
+  name: 'HomePage',
+  data() {
+    return {
+      items: [
+        { scrapName: 'Scrap Name', date: '20/June/2024', fee: 10 },
+        { scrapName: 'Scrap Name', date: '20/June/2024', fee: 2 },
+        { scrapName: 'Scrap Name', date: '20/June/2024', fee: 2 },
+        { scrapName: 'Scrap Name', date: '20/June/2024', fee: 1 },
+        { scrapName: 'Scrap Name', date: '20/June/2024', fee: 0.5 },
+        { scrapName: 'Scrap Name', date: '20/June/2024', fee: 1 },
+        { scrapName: 'Scrap Name', date: '20/June/2024', fee: 1 },
+        { scrapName: 'Scrap Name', date: '20/June/2024', fee: 1 },
+        { scrapName: 'Scrap Name', date: '20/June/2024', fee: 1 },
+        { scrapName: 'Scrap Name', date: '20/June/2024', fee: 1 },
+        { scrapName: 'Scrap Name', date: '20/June/2024', fee: 1 },
+        { scrapName: 'Scrap Name', date: '20/June/2024', fee: 1 },
+        { scrapName: 'Scrap Name', date: '20/June/2024', fee: 1 },
+        { scrapName: 'Scrap Name', date: '20/June/2024', fee: 1 },
+        { scrapName: 'Scrap Name', date: '20/June/2024', fee: 1 },
+      ],
+      companies: [
+        {
+          name: 'Company 1',
+          service: 'service 1',
+          address: 'Phnom Penh',
+          tel: '012 345 6789',
+          image: 'company-logo.png',
+          description: 'Description for Company 1'
+        },
+        {
+          name: 'Company 2',
+          service: 'service 2',
+          address: 'Phnom Penh',
+          tel: '012 345 6789',
+          image: 'company-logo.png',
+          description: 'Description for Company 2'
+        },
+        {
+          name: 'Company 2',
+          service: 'service 2',
+          address: 'Phnom Penh',
+          tel: '012 345 6789',
+          image: 'company-logo.png',
+          description: 'Description for Company 2'
+        },
+        {
+          name: 'Company 2',
+          service: 'service 2',
+          address: 'Phnom Penh',
+          tel: '012 345 6789',
+          image: 'company-logo.png',
+          description: 'Description for Company 2'
+        },
+        {
+          name: 'Company 2',
+          service: 'service 2',
+          address: 'Phnom Penh',
+          tel: '012 345 6789',
+          image: 'company-logo.png',
+          description: 'Description for Company 2'
+        },
+      ]
     }
-  .d-flex{
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
   }
-  #bg-blur {
-  background-size: cover;
-  background-position: center;
-  backdrop-filter: blur(5px);
-  width: 100%;
-  height: 100vh;
+}
+</script>
 
-}
-#misson{
-  position: absolute;
-  top: 20%;
-  left: 30%;
-  transform: translate(-50%, -40%);
-  width: 100%;
-  height: 100%;
-  padding: 0px;
- 
-}
-#blur-text{
-  font-size: 8px;
-  width: 100%;
-  height: 100%;
+<style scoped>
+.carousel-item img {
+  height: 600px;
+  object-fit: cover;
+  object-position: center;
+  filter: brightness(70%);
 }
 
-#imag-right{
-  border-bottom: 5px solid hwb(152 1% 48%);
-  max-width: 100%;
-  max-height: 100%
-}
-  .col-12.col-md-4 {
-    /* margin-bottom: 1rem; */
-    margin-left: 0%!important;
-  }
-
-  .col-12.col-md-7 {
-    margin-left: 0% !important;
-  }
+.list-container {
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  gap: 1rem;
 }
 
-@media (min-width: 768px) and (max-width: 1023px) {
-  .position-absolute.top-50.start-10.text-white.w-50.d-none.d-md-block {
-    top: 50% !important;
-    left: 7% !important;
-    width: 60% !important;
+.company-card {
+  border-radius: 0.5rem;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  transition: transform 0.3s ease-in-out, box-shadow 0.3s ease-in-out, border-color 0.3s ease-in-out;
+  position: relative;
+  overflow: hidden;
+  border: 2px solid thick;
+}
+
+
+.company-card:hover {
+  transform: translateY(-10px);
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  border-color: #007bff;
+}
+
+
+.company-card img {
+  width: 110px;
+  height: 110px;
+  object-fit: fill;
+  
+}
+
+.text-card {
+  line-height: 1px;
+  font-size: 12px;
+}
+
+@media (min-width: 1200px) {
+  .list-container {
+    grid-template-columns: repeat(5, 1fr);
+  }
+}
+
+@media (min-width: 768px) and (max-width: 1199px) {
+  .carousel-item img {
+    height: 100%;
   }
 
-  .col-12.col-md-4 {
-    margin-bottom: 1rem;
+  .list-container {
+    grid-template-columns: repeat(3, 1fr);
+  }
+}
+
+@media (max-width: 767px) {
+  .carousel-item img {
+    height: 200px;
   }
 
-  .col-12.col-md-7 {
-    margin-left: 0.5rem !important;
+  .list-container {
+    grid-template-columns: repeat(2, 1fr);
   }
-  #bg-blur {
-  background-size: cover;
-  background-position: center;
-  backdrop-filter: blur(9px);
-  width: 100%;
-  height: 100vh;
-  }
-  #misson {
-  position: absolute;
-  top: 40%;
-  left: 30%;
-  transform: translate(-50%, -45%);
-  width: 100%;
-  height: 100%;
-  font-size:8px;
+  .company-card img {
+  width: 70px;
+  height: 70px;
+  
 }
-#blur-text{
-  font-size: 25px;
-  width: 100%;
+.company-card .company-info h5{
+  margin-top: 5px;
+  font-size: 12px;
+  font-weight: bold;
 }
-  #imag-right {
-    border-bottom: 5px solid hwb(152 1% 48%);
-    max-width: 100%;
-    max-height: 100%
-  }
-  #img-r2{
-    border-bottom: 5px solid hwb(152 1% 48%);
-    max-width: 100%;
-    max-height: 100;
-  }
-  .col-md-4{
-    line-height: 2.5px;
-  }
-  #question{
-    font-size: 25px;
-  }
 }
 </style>
